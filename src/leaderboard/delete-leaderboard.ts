948cdac8a49a9e0583c3e21227126808652ea766
Mai Nguyen Khang <khang@Mais-Mac-mini.local>
2024-08-14 14:10:26 +0700

    Update delete leaderboard method

---
+++ src/leaderboard/delete-leaderboard.ts
@@ -0,0 +1,23 @@
+
+import config from "../config";
+import { LeaderboardManager, InitLeaderboardParams } from '@myria/leaderboard-ts-sdk';
+  
+  (async (): Promise<void> => {
+    const environment = config.env;
+
+    const developerApiKey = config.developer_api_key;
+    const initLeaderboardParams: InitLeaderboardParams = {
+        env: environment, // Use your desired environment type (STAGING / PREPROD / PROD)
+        apiKey: developerApiKey, // Your Developer API Key
+    };
+      
+    const leaderboardManager = new LeaderboardManager(initLeaderboardParams);
+    const leaderboardId = 51;
+  
+    console.log("Deleting the leaderboard...");
+    const deleteLeaderboardResp = await leaderboardManager.deleteLeaderboardById(leaderboardId);
+  
+    console.log("Deleted leaderboard response:");
+    console.log(JSON.stringify(deleteLeaderboardResp, null, 2));
+  })();
+  
